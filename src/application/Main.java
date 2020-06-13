007e6b1c7d2848f8d2f3c67c0d9e5d796cdd032e
MatheusAprigio <matheusaprig@Gmail.com>
2020-06-12 20:43:20 -0400

    Add Process Tab

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-			Pane root = FXMLLoader.load(getClass().getResource("TaskManagerFXML.fxml")); 
+			Pane root = FXMLLoader.load(getClass().getResource("TaskManager.fxml")); 
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/controller/SystemInfoController.java
+++ src/controller/SystemInfoController.java
@@ -1,25 +1,35 @@
 package controller;
 
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import oshi.SystemInfo;
 import oshi.hardware.GraphicsCard;
 import oshi.hardware.PhysicalMemory;
 import oshi.software.os.OSFileStore;
+import oshi.software.os.OSProcess;
+import oshi.software.os.OSService;
 
 
 public class SystemInfoController implements Initializable{
 
 	SystemInfo systemInfo = new SystemInfo();
-	
+
 	@FXML
 	AnchorPane SystemInfoAP;
+	
 	@FXML
 	Text ManufacturerTX;
 
@@ -31,42 +41,74 @@ public class SystemInfoController implements Initializable{
 
 	@FXML
 	Text FileStoreTX;
-	
+
 	@FXML
 	Text VersionTX;
-	
+
 	@FXML
 	Text PCManufacturerTX;
-	
+
 	@FXML
 	Text PCModelTX;
-	
+
 	@FXML
 	Text PCSerialNumberTX;
-	
+
 	@FXML
 	Text GraphicCardTX;
-	
+
 	@FXML
 	Text ProcessorTX;
 	
+	@FXML
+	TableView<OSProcess> processTBL;
+	
+	@FXML
+	TableColumn<OSProcess, String> ProcessNameCLN = new TableColumn<>("Name");
+	@FXML
+	TableColumn<OSProcess, String> PriorityCLN = new TableColumn<>("Priority");
+	@FXML
+	TableColumn<OSProcess, String> ProcessUserCLN = new TableColumn<>("User");
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
 		ManufacturerTX.setText(systemInfo.getOperatingSystem().getManufacturer());
 		FamilyTX.setText(systemInfo.getOperatingSystem().getFamily());
 		VersionTX.setText(systemInfo.getOperatingSystem().getVersionInfo().toString());
-		
+
 		FileStoreTX.setText(formatOsFileStore());
 		MemoryTX.setText(formatMemory());
-		MemoryTX.setWrappingWidth(SystemInfoAP.getWidth());
 		
 		PCManufacturerTX.setText(systemInfo.getHardware().getComputerSystem().getManufacturer());
 		PCModelTX.setText(systemInfo.getHardware().getComputerSystem().getModel());
 		PCSerialNumberTX.setText(systemInfo.getHardware().getComputerSystem().getSerialNumber());
-		
+
 		GraphicCardTX.setText(formatGraphicCard()); 
 		ProcessorTX.setText(systemInfo.getHardware().getProcessor().toString());
+		
+		getProcesses();
+	}
+
+	public void getProcesses(){
+	
+	List<OSProcess> serviceList = systemInfo.getOperatingSystem().getProcesses();
+	ObservableList<OSProcess> observableList = FXCollections.observableArrayList();
+	
+	for(OSProcess osService : serviceList) {
+		observableList.add(osService);
+	}
+	
+	ProcessNameCLN.setCellValueFactory(cellData -> 
+    new SimpleStringProperty(cellData.getValue().getName()));
+	
+	PriorityCLN.setCellValueFactory(cellData -> 
+    new SimpleStringProperty(String.valueOf(cellData.getValue().getPriority())));
+	
+	ProcessUserCLN.setCellValueFactory(cellData -> 
+    new SimpleStringProperty(cellData.getValue().getUser()));
+	
+	processTBL.setItems(observableList);
+	
 	}
 
 	public String formatOsFileStore() {
@@ -92,7 +134,7 @@ public class SystemInfoController implements Initializable{
 		}
 		return memoryStr;
 	}
-	
+
 	public String formatGraphicCard() {
 		List<GraphicsCard> graphicsCards = systemInfo.getHardware().getGraphicsCards();
 		String graphicCardStr= "";
@@ -103,7 +145,7 @@ public class SystemInfoController implements Initializable{
 					"   " + graphicCard.getVersionInfo() + 
 					"\n";
 		}
-		
+
 		return graphicCardStr;
 	}
 }
